1dc51afdcc6f098733b0bd4903d09d3925c54c04
Felix <felix.delrio.1@gmail.com>
2019-06-27 14:05:24 +0200

    @input y @output
    Pasar template del <li> a un component hijo, ademas de crear una funcion en el hijo con la que el padre posts, recibe a traves de un evento de click

---
+++ Ionic/02-angularbases/src/app/pages/posts/post/post.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() post: any;
+  @Output() ClickPost = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    this.ClickPost.emit( this.post.id );
+  }
+
+}
